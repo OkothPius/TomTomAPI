b44266c17ce0916ef84a24cf331070fec1c9bbf0
Okoth Pius <orukopius8@gmail.com>
2021-10-05 11:41:10 +0300

    feat: update

--- notebooks/traffic.py
+++ notebooks/traffic.py
@@ -1,59 +1,34 @@
 import requests
 import matplotlib.pyplot as plt
 
-Your_Api_Key = 'a2cvZL5Hn6VUHWOBOJYMKnqD3122VWnY'
-site_url = 'https://api.tomtom.com/traffic/services/5/incidentDetails?key=a2cvZL5Hn6VUHWOBOJYMKnqD3122VWnY&bbox=4.8854592519716675,52.36934334773164,4.897883244144765,52.37496348620152'
+# Your_Api_Key = 'a2cvZL5Hn6VUHWOBOJYMKnqD3122VWnY'
+# site_url = 'https://api.tomtom.com/traffic/services/5/incidentDetails?key=a2cvZL5Hn6VUHWOBOJYMKnqD3122VWnY&bbox=4.8854592519716675,52.36934334773164,4.897883244144765,52.37496348620152'
+plt.style.use('seaborn')
+
+site_url = 'https://api.tomtom.com/traffic/services/5/incidentDetails?key=a2cvZL5Hn6VUHWOBOJYMKnqD3122VWnY&bbox=4.8854592519716675,52.36934334773164,4.897883244144765,52.37496348620152&fields={incidents{type,geometry{type,coordinates},properties{id,iconCategory,magnitudeOfDelay,events{description,code,iconCategory},startTime,endTime,from,to,length,delay,roadNumbers,timeValidity,aci{probabilityOfOccurrence,numberOfReports,lastReportTime},tmc{countryCode,tableNumber,tableVersion,direction,points{location,offset}}}}}&language=en-GB&t=1633421340&categoryFilter=0,1,2,3,4,5,6,7,8,9,10,11,14&timeValidityFilter=present'
+
 response = requests.get(site_url)
 response_json = response.json()
 
 
-print(response_json['incidents'][0])
+print(response_json)
+incident = response_json['incidents'][0]
+# print(incident)
 
+property = incident['properties']
 
-site_url = 'https://api.tomtom.com/traffic/services/5/incidentDetails?key=Your_Api_Key&bbox=4.8854592519716675,52.36934334773164,4.897883244144765,\
-52.37496348620152&fields={incidents{type,geometry{type,coordinates},properties{id,iconCategory,magnitudeOfDelay,events{description,code,iconCategory},\
-startTime,endTime,from,to,length,delay,roadNumbers,timeValidity,aci{probabilityOfOccurrence,numberOfReports,lastReportTime},tmc{countryCode,tableNumber,\
-tableVersion,direction,points{location,offset}}}}}&language=en-GB&t=1111&categoryFilter=6&timeValidityFilter=present'
+traffic_level, traffic_time = [],[]
+for item in property:
+    traffic_level.append(item['delay'])
+    traffic_time.append(item['startTime'])
 
-site_url = 'https://api.tomtom.com/traffic/services/5/incidentDetails?key=Your_Api_Key&bbox=4.8854592519716675,52.36934334773164,4.897883244144765,\
-52.37496348620152&fields={incidents{type,geometry{type,coordinates},properties{id,iconCategory,magnitudeOfDelay,events{description,code,iconCategory},\
-startTime,endTime,from,to,length,delay,roadNumbers,timeValidity,aci{probabilityOfOccurrence,numberOfReports,lastReportTime},tmc{countryCode,tableNumber,\
-tableVersion,direction,points{location,offset}}}}}&language=en-GB&t=1111&categoryFilter=6&timeValidityFilter=present'
+plt.barh(traffic_level, traffic_time)
 
-site_url = 'https://api.tomtom.com/traffic/services/5/incidentDetails?key=Your_Api_Key&bbox=4.8854592519716675,52.36934334773164,4.897883244144765,52.37496348620152&fields={incidents{type,geometry{type,coordinates},properties{id,iconCategory,magnitudeOfDelay,events{description,code,iconCategory},startTime,endTime,from,to,length,delay,roadNumbers,timeValidity,aci{probabilityOfOccurrence,numberOfReports,lastReportTime},tmc{countryCode,tableNumber,tableVersion,direction,points{location,offset}}}}}&language=en-GB&t=1111&categoryFilter=8&timeValidityFilter=present'
+plt.title('Traffic Levels from Hartenstraat to Raadhuisstraat')
+plt.xlabel('Time in the last 24hrs')
 
+plt.tight_layout()
+plt.show()
 
-
-    {
-      "incidents" : [
-        {
-          "type" : "Feature",
-          "properties" : {
-            "id":"4819f7d0a15db3d9b0c3cd9203be7ba5",
-            "iconCategory" : 8,
-            "magnitudeOfDelay" : 4,
-            "startTime" : "2021-02-02T15:37:00Z",
-            "endTime" : "2021-04-30T22:00:00Z",
-            "from" : "Paleisstraat",
-            "to" : "Rosmarijnsteeg",
-            "length" : 238.553,
-            "delay" : 0,
-            "roadNumbers" : [],
-            "timeValidity" : "present",
-            "events" : [
-              {
-                "code" : 401,
-                "description" : "Closed",
-                "iconCategory" : 8
-              }
-            ],
-            "aci" : null,
-            "tmc" : null
-          },
-          "geometry" : {
-            "type" : "LineString",
-            "coordinates" : [[4.8905266414,52.3725919469],[4.8905306647,52.3725356560],[4.8905360291,52.3724806443],[4.8905387113,52.3724028603],[4.8905440757,52.3723505607],[4.8905467579,52.3722754886],[4.8905574868,52.3721722195],[4.8905762622,52.3719066767],[4.8905963788,52.3716639050],[4.8905936966,52.3715244540],[4.8905749211,52.3714278871],[4.8905440757,52.3713393544],[4.8905065248,52.3712669418],[4.8904555628,52.3711703743],[4.8904166708,52.3711100387],[4.8903268168,52.3709759593],[4.8901725898,52.3707653720],[4.8900062928,52.3705816510],[4.8899472842,52.3705320104]]
-          }
-        }
-      ]
-    }
+# print(incident['properties'])
+response.status_code
